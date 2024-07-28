9e360ffe02e8d5c2cba255276e82e0ea5ba8c553
Ami Nice <amimakeup@gmail.com>
2024-07-28 19:47:30 +0300

    assignment_lesson_5

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -1,13 +1,8 @@
-# File: project/ui/user_interaction.py
 
-# Get player name
-name = input("Hello, whats your name\n")
+name = input("Hello, whats your name")
 
-# Get board size
-board_size = int(input(f"{name}, please choose board size\n"))
+board_size = int(input(f"{name}, please choose board size"))
 
-# Get number of mines
-number_of_mines = int(input(f"{name}, for board size {board_size}, choose number of mines to allocate\n"))
+number_of_mines = int(input(f"{name}, for board size {board_size}, choose number of mines to allocate"))
 
-# Print summary
 print(f"{name}, the board size is: {board_size}, number of mines is: {number_of_mines}. ENJOY!")
