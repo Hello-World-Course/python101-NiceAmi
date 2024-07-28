33ed985816e8214490168060bc1465e563e64cdf
Ami Nice <amimakeup@gmail.com>
2024-07-28 22:16:49 +0300

    assignment_lesson_5

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -1,23 +1,46 @@
-# name = None
-# board_size = None
-# number_of_mines = None
+name = None
+board_size = None
+number_of_mines = None
 
-
-name = input("Hello, what's your name: ")
-if len(name) <= 2:
-    print("Your name is too short")
+# Get player name
+name_input = input("Hello, whats your name")
+if len(name_input) > 2:
+    name = name_input
 else:
-    board_size_input = input(f"{name}, please choose board size: ")
-    if not board_size_input.isdigit() or not (0 < int(board_size_input) < 26):
-        print(f"{name}, you have entered illegal board size")
-    else:
-        board_size = int(board_size_input)
-        max_mines = (board_size * board_size) // 2
-        number_of_mines_input = input(f"{name}, for board size {board_size}, choose number of mines to allocate: ")
+    print("Your name is too short")
+    exit()
 
-        if not number_of_mines_input.isdigit() or not (0 < int(number_of_mines_input) <= max_mines):
-            print(f"{name}, you have entered illegal number of mines")
+# Get board size
+if name:
+    board_size_input = input(f"{name}, please choose board size")
+    try:
+        board_size_int = int(board_size_input)
+        if 0 < board_size_int < 26:
+            board_size = board_size_int
         else:
-            number_of_mines = int(number_of_mines_input)
+            print(f"{name}, you have entered illegal board size")
+            exit()
+    except ValueError:
+        print(f"{name}, you have entered illegal board size")
+        exit()
 
-            print(f"{name}, the board size is: {board_size}, number of mines is: {number_of_mines}. ENJOY!")
+# Get number of mines
+if board_size:
+    mines_input = input(f"{name}, for board size {board_size}, choose number of mines to allocate")
+    try:
+        mines_int = int(mines_input)
+        max_mines = (board_size * board_size) // 2
+        if 0 < mines_int <= max_mines:
+            number_of_mines = mines_int
+        else:
+            print(f"{name}, you have entered illegal number of mines")
+            exit()
+    except ValueError:
+        print(f"{name}, you have entered illegal number of mines")
+        exit()
+
+# Print final status
+if name and board_size and number_of_mines:
+    print(f"name = \"{name}\"")
+    print(f"board_size = {board_size}")
+    print(f"number_of_mines = {number_of_mines}")
