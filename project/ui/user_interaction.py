77b7fc714256d7f64845f779f58c98013308fb8b
Ami Nice <amimakeup@gmail.com>
2024-07-28 19:36:31 +0300

    created print "Board game!"

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -1,15 +1,9 @@
 # THIS CODE IS WRONG, FIX IT AND ADD NEW CODE
 # Corrected code
-name = input("Hello, what's your name? ")
-board_size = int(input(f"{name}, please choose board size: "))
-number_of_mines = int(input(f"{name}, for board size {board_size}, choose number of mines to allocate: "))
+# Corrected code
+name = input("Hello, what's your name\ninput:> ")
+board_size = int(input(f"{name}, please choose board size\ninput:> "))
+number_of_mines = int(input(f"{name}, for board size {board_size}, choose number of mines to allocate:\ninput:> "))
 
 print(f"{name}, the board size is: {board_size}, number of mines is: {number_of_mines}. ENJOY!")
 
-
-
-name = input("Hello, whats your name")
-board_size = int(input(f"{name}, please choose board size"))
-number_of_mines = int(input(f"{name}, for board size {board_size}, choose number of mines to allocate"))
-
-print(f"{name}, the board size is: {board_size}, number of mines is: {number_of_mines}, ENJOY!")
