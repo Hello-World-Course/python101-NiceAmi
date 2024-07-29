587771df20045c6073ba33d6a6b3fc5946c38f59
Ami Nice <amimakeup@gmail.com>
2024-07-30 00:40:29 +0300

    assignment_lesson_8

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -2,26 +2,24 @@ name = None
 board_size = None
 number_of_mines = None
 
-name_input = input("Hello, whats your name\n")
-if len(name_input) > 2:
-    name = name_input
-    board_size_input = input(f"{name}, please choose board size\n")
-    if board_size_input.isdigit() and 0 < int(board_size_input) < 26:
+input_name = input("Hello, what's your name: ")
+
+if len(input_name) <= 2:
+    print("Your name is too short")
+else:
+    name = input_name
+    board_size_input = input(f"{name}, please choose board size: ")
+
+    if not board_size_input.isdigit() or not (0 < int(board_size_input) < 26):
+        print(f"{name}, you have entered illegal board size")
+    else:
         board_size = int(board_size_input)
         max_mines = (board_size * board_size) // 2
-        number_of_mines_input = input(f"{name}, for board size {board_size}, choose number of mines to allocate\n")
-        if number_of_mines_input.isdigit() and 0 < int(number_of_mines_input) <= max_mines:
-            number_of_mines = int(number_of_mines_input)
-        else:
-            print(f"{name}, you have entered illegal number of mines")
-            name = None
-            board_size = None
-    else:
-        print(f"{name}, you have entered illegal board size")
-        name = None
-else:
-    print("Your name is too short")
 
-print(f"name = {name!r}")
-print(f"board_size = {board_size}")
-print(f"number_of_mines = {number_of_mines}")
+        number_of_mines_input = input(f"{name}, for board size {board_size}, choose number of mines to allocate: ")
+
+        if not number_of_mines_input.isdigit() or not (0 < int(number_of_mines_input) <= max_mines):
+            print(f"{name}, you have entered illegal number of mines")
+        else:
+            number_of_mines = int(number_of_mines_input)
+            print(f"{name}, the board size is: {board_size}, number of mines is: {number_of_mines}. ENJOY!")
