eb0097f08fc4c9684fa9a16b7642fbc4453af297
Ami Nice <amimakeup@gmail.com>
2024-07-29 14:35:11 +0300

    assignment_lesson_5

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -2,7 +2,6 @@ name = None
 board_size = None
 number_of_mines = None
 
-# Get player name
 name_input = input("Hello, whats your name")
 if len(name_input) > 2:
     name = name_input
@@ -10,7 +9,6 @@ else:
     print("Your name is too short")
     exit()
 
-# Get board size
 if name:
     board_size_input = input(f"{name}, please choose board size")
     try:
@@ -24,7 +22,6 @@ if name:
         print(f"{name}, you have entered illegal board size")
         exit()
 
-# Get number of mines
 if board_size:
     mines_input = input(f"{name}, for board size {board_size}, choose number of mines to allocate")
     try:
@@ -39,7 +36,6 @@ if board_size:
         print(f"{name}, you have entered illegal number of mines")
         exit()
 
-# Print final status
 if name and board_size and number_of_mines:
     print(f"name = \"{name}\"")
     print(f"board_size = {board_size}")
