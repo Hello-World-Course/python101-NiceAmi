a76c04a4be951c0115978dea4a64a9c232688aee
Ami Nice <amimakeup@gmail.com>
2024-07-31 21:19:59 +0300

    assignment_lesson_9

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -21,13 +21,11 @@ def is_number_of_mines_valid(board_size, number_of_mines):
 
 
 def register_user():
-    # Name input
     name = input("Hello, whats your name? ")
     if not is_name_valid(name):
         print("Your name is too short")
         return None
 
-    # Board size input
     board_size = input(f"{name}, please choose board size: ")
     if not is_board_size_valid(board_size):
         print(f"{name}, you have entered illegal board size")
@@ -45,6 +43,5 @@ def register_user():
     return name, board_size, number_of_mines
 
 
-# Test the function
 user_name, user_board_size, user_num_mines = register_user()
 print(f"name:{user_name}, board size:{user_board_size}, number of mines:{user_num_mines}")
